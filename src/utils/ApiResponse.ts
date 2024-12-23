8c919de7da1d611f326eb2f7a013966de5650506
Hemant Kaushik <hemantKaushik166@gmail.com>
2024-12-23 22:44:31 +0530

    Add utils class for handling errors, api response and async handler

---
+++ src/utils/ApiResponse.ts
@@ -0,0 +1,14 @@
+export class ApiResponse {
+    // Declare properties first
+    statusCode: number;
+    data: any;
+    message: string;
+    success: boolean;
+
+    constructor(statusCode: number, data: any, message: string = "Success") {
+        this.statusCode = statusCode;
+        this.data = data;
+        this.message = message;
+        this.success = statusCode < 400;
+    }
+}
